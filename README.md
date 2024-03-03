ce88cc4f343e63a4dd899e267e31f50f578a0c51
Amir Ali Karimi <amr.karm_0121@yahoo.com>
2024-03-03 19:06:49 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+### Financial Textual Data Scraper
+
+#### News and Tweets
+
+#### Websites
+
+1. [Stocktwits](https://Stocktwits.com)
+	- [x] /api/2/streams/trending.json
+	- TO-DO:
+		- Scrape other apis such as:
+			- [ ] https://api.stocktwits.com/api/2/trending_messages/region/ALL?filter=all&limit=20&page_num=1
+			- [ ] https://api.stocktwits.com/api/2/trending_messages/symbol_trending?filter=all&limit=20&page_num=1
+		- Scrape based on symbol
+
+2. [Fortune](https://fortune.com/)
+	- [ ] /api/page/latest/all
+	- [ ] /api/page/section/tech
+	- [ ] /api/page/section/finance
+	- [ ] /api/page/section/leadership
+	- [ ] /api/page/section/well
---
+++ src/stocktwits/latest-trending.ts
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { createObjectCsvWriter} from "csv-writer";
+
+const baseUrl = "https://api.stocktwits.com/api/2/streams/trending.json";
+
+const csvWriter = createObjectCsvWriter( {
+	path: `data/stocktwits/trending-${ Date.now()}.csv`,
+	header: [
+		{ id: "id", title: "id"},
+		{ id: "created_at", title: "created_at"},
+		{ id: "username", title: "username"},
+		{ id: "user_id", title: "user_id"},
+		{ id: "body", title: "body"},
+		{ id: "symbols", title: "symbols"},
+		{ id: "likes", title: "likes"},
+		{ id: "reshares", title: "reshares"},
+		{ id: "sentiment", title: "sentiment"}
+	]
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getTweet = async ( max: number): Promise<any> => {
+	console.info( `Max: ${ max}`);
+
+	const url = ( max === 0) ? baseUrl : `${ baseUrl}?max=${ max}`;
+
+	let res;
+	try {
+		res = await axios.get( url);
+	} catch( err) {
+		console.error( "axios error - retrying...");
+		return await getTweet( max);
+	}
+
+	const data = res.data;
+
+	if( data.messages.length === 0)
+		return;
+
+	const maxCursor = data?.cursor?.max;
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const result = data.messages.map( ( msg: any) => {
+		return {
+			id: msg.id,
+			created_at: msg.created_at,
+			username: msg.user.username,
+			user_id: msg.user.id,
+			body: msg.body.replaceAll( ",", ""),
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			symbols: msg.symbols.map( ( s: any) => s.symbol).join( "|"),
+			likes: msg.likes?.total || 0,
+			reshares: msg.reshares?.reshared_count || 0,
+			sentiment: msg?.entities?.sentiment?.basic || null,
+		};
+	});
+
+	await csvWriter.writeRecords( result);
+
+	if( maxCursor)
+		return await getTweet( maxCursor);
+};
+
+let max = parseInt( process.argv[ 2]);
+
+( async () => {
+	console.info( "Starting Scraping...");
+	if( !max) {
+		max = 0;
+	} else {
+		console.info( "Using Provided argument as Max parameter");
+	}
+
+	console.log( `value of Max parameter: ${ max}`);
+
+	await getTweet( max);
+})();
